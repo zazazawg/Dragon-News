7e90f2ed41d9365af0781c4a87ec9cab0437c1e9
ferdouszihad <f.zihad333@gmail.com>
2025-04-28 03:22:40 +0600

    newscard added

---
+++ src/components/NewsCard.jsx
@@ -0,0 +1,82 @@
+import { FaEye, FaStar, FaShareAlt, FaRegBookmark } from "react-icons/fa";
+
+const NewsCard = ({ news }) => {
+  const { title, author, thumbnail_url, details, rating, total_view, tags } =
+    news;
+
+  const formattedDate = new Date(
+    news.author.published_date
+  ).toLocaleDateString();
+
+  return (
+    <div className="card bg-base-100 shadow-md mb-6">
+      {/* Author + Share */}
+      <div className="flex bg-base-200 justify-between items-center p-4">
+        <div className="flex items-center gap-3">
+          <div className="avatar">
+            <div className="w-10 rounded-full">
+              <img src={author.img} alt={author.name} />
+            </div>
+          </div>
+          <div>
+            <h2 className="font-bold text-sm">{author.name}</h2>
+            <p className="text-xs text-gray-500">{formattedDate}</p>
+          </div>
+        </div>
+        <button className="text-gray-500 hover:text-primary flex gap-1">
+          <FaRegBookmark></FaRegBookmark>
+          <FaShareAlt />
+        </button>
+      </div>
+
+      {/* Title */}
+      <div className="px-4 py-4">
+        <h2 className="text-lg font-bold text-primary  cursor-pointer">
+          {title}
+        </h2>
+      </div>
+
+      {/* Image */}
+      <div className="px-4 py-2">
+        <img
+          src={thumbnail_url}
+          alt={title}
+          className="w-full h-48 object-cover rounded-md"
+        />
+      </div>
+
+      {/* Details */}
+      <div className="px-4  text-accent">
+        {details.length > 200 ? (
+          <>
+            {details.slice(0, 200)}...
+            <span className="text-primary font-semibold cursor-pointer hover:underline">
+              Read More
+            </span>
+          </>
+        ) : (
+          details
+        )}
+      </div>
+
+      {/* Footer */}
+      <div className="flex justify-between items-center px-4 py-3 border-t border-base-200 mt-3">
+        {/* Rating */}
+        <div className="flex items-center gap-1 text-orange-400">
+          {Array.from({ length: rating.number }).map((_, i) => (
+            <FaStar key={i} />
+          ))}
+          <span className="ml-2 text-gray-600">{rating.number}</span>
+        </div>
+
+        {/* Views */}
+        <div className="flex items-center gap-2 text-gray-500">
+          <FaEye />
+          <span>{total_view}</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NewsCard;
--- src/pages/CategoryNews.jsx
+++ src/pages/CategoryNews.jsx
@@ -1,29 +1,44 @@
 import React, { useEffect, useState } from "react";
 import { useLoaderData, useParams } from "react-router";
+import NewsCard from "../components/NewsCard";
 
 const CategoryNews = () => {
-  const { id } = useParams();
+  const { id } = useParams(); //string
   const data = useLoaderData();
 
   const [categoryNews, setCategoryNews] = useState([]);
 
-  // console.log(id, data);
   useEffect(() => {
     if (id == "0") {
       setCategoryNews(data);
-      return;
     } else if (id == "1") {
       const filteredNews = data.filter(
         (news) => news.others.is_today_pick == true
       );
+
       setCategoryNews(filteredNews);
     } else {
       const filteredNews = data.filter((news) => news.category_id == id);
-      // console.log(filteredNews);
       setCategoryNews(filteredNews);
     }
-  }, [data, id]);
-  return <div>Total {categoryNews.length} news Found</div>;
+  }, [id, data]);
+
+  // console.log(id, data);
+
+  return (
+    <div>
+      <h2 className="font-bold mb-5">
+        Total <span className="text-secondary">{categoryNews.length}</span> news
+        Found
+      </h2>
+
+      <div className="grid grid-cols-1 gap-5">
+        {categoryNews.map((news) => (
+          <NewsCard key={news.id} news={news}></NewsCard>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default CategoryNews;
