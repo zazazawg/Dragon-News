82f86e7ed95f68968da37b69db53c189a055d290
ferdouszihad <f.zihad333@gmail.com>
2025-04-28 17:59:21 +0600

    added navigate to  category

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import { Navigate } from "react-router";
 
 const Home = () => {
-  return <div>Home Page</div>;
+  return <Navigate to="/category/1"></Navigate>;
 };
 
 export default Home;
